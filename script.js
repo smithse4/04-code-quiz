6ea67ebb32cbf9d440ca2e1c15430547cb14e5be
Stephanie smith <smithse4@gmail.com>
2020-03-09 22:57:51 -0400

    started adding functions in JS and tested start button event listener with console log

--- script.js
+++ script.js
@@ -1,32 +1,53 @@
-var questionOneEl = document.quiz.questionOne.value;
-var questionTwoEl = document.quiz.questionTwo.value;
-var questionThreeEl = document.quiz.questionThree.value;
-var questionFourEl = document.quiz.questionFour.value;
-var questionFiveEl = document.quiz.questionFive.value;
+var startButton = document.getElementById("btn-start")
+
+// var questionOneEl = document.quiz.questionOne.value;
+// var questionTwoEl = document.quiz.questionTwo.value;
+// var questionThreeEl = document.quiz.questionThree.value;
+// var questionFourEl = document.quiz.questionFour.value;
+// var questionFiveEl = document.quiz.questionFive.value;
 
 var correct = 0;
 
-if (questionOneEl ==="Morocco") {
-    correct++;
-}
-if (questionTwoEl ==="goingPlaces") {
-    correct++;
-}
-if (questionThreeEl ==="document") {
-    correct++;
-}
-if (questionFourEl ==="API = application programming index") {
-    correct++;
-}
-if (questionFiveEl ==="addEventListener") {
-    correct++;
+startButton.addEventListener("click", startQuiz)
+
+
+function startQuiz() {
+    console.log("Start Quiz")
+
 }
 
 
+function nextQuestion() {
+
+
+}
+
+function selectAnswer(){
+
+
+}
+
+// if (questionOneEl ==="Morocco") {
+//     correct++;
+// }
+// if (questionTwoEl ==="goingPlaces") {
+//     correct++;
+// }
+// if (questionThreeEl ==="document") {
+//     correct++;
+// }
+// if (questionFourEl ==="API = application programming index") {
+//     correct++;
+// }
+// if (questionFiveEl ==="addEventListener") {
+//     correct++;
+// }
 
 
 
 
 
-document.getElementById("after-submit").style.visibility = "visible";
-document.getElementById("number_correct").innerHTML = "You got " + correct + " out of 5 questions correct";
+
+
+// document.getElementById("after-submit").style.visibility = "visible";
+// document.getElementById("number_correct").innerHTML = "You got " + correct + " out of 5 questions correct";
