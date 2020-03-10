01648b26738af6ffbb51e7866d7f53bf10c8ce07
Stephanie smith <smithse4@gmail.com>
2020-03-09 22:01:59 -0400

    created hidden code to be revealed by JS once submit button is clicked and created variables for 5 quiz questions

---
+++ script.js
@@ -0,0 +1,32 @@
+var questionOneEl = document.quiz.questionOne.value;
+var questionTwoEl = document.quiz.questionTwo.value;
+var questionThreeEl = document.quiz.questionThree.value;
+var questionFourEl = document.quiz.questionFour.value;
+var questionFiveEl = document.quiz.questionFive.value;
+
+var correct = 0;
+
+if (questionOneEl ==="Morocco") {
+    correct++;
+}
+if (questionTwoEl ==="goingPlaces") {
+    correct++;
+}
+if (questionThreeEl ==="document") {
+    correct++;
+}
+if (questionFourEl ==="API = application programming index") {
+    correct++;
+}
+if (questionFiveEl ==="addEventListener") {
+    correct++;
+}
+
+
+
+
+
+
+
+document.getElementById("after-submit").style.visibility = "visible";
+document.getElementById("number_correct").innerHTML = "You got " + correct + " out of 5 questions correct";
