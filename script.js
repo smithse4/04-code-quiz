9daa20a2c305f35b77e38334503ee1cff19e2bfd
Stephanie smith <smithse4@gmail.com>
2020-03-11 09:48:49 -0400

    created functioning countdown that needs to be connected to start quiz button

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
+// Start, question, and answer buttons
 var startButton = document.getElementById("btn-start");
 var startTextEl = document.getElementById("start-text");
 var answerButton = document.querySelector(".answer-btn");
 var questionContainerEl = document.getElementById("question-container");
-
 var questionEl = document.getElementById("question");
 var answerBtnEl = document.getElementById("answer-btns");
 
+
+
 let questionIndex
 
-timeEl = document.getElementById("time");
-var timeLeft = 60;
 
 
+// Respond to buttons being vlicked
 startButton.addEventListener("click", startQuiz)
 answerButton.addEventListener("click", () => {
     questions[i]++;
@@ -25,8 +26,22 @@ function startQuiz() {
     questionsIndex = 0;
     questionContainerEl.classList.remove("hide");
     showQuestion();
-    setTime():
+   
+    
 }
+
+// Timer -- need to get set to start button
+var timeleft = 60;
+var timer = setInterval(function(){
+if(timeleft <= 0){
+    clearInterval(timer);
+    document.getElementById("time").innerHTML = "Quiz over";
+} else {
+    document.getElementById("time").innerHTML = "Time remaining: " + timeleft;
+}
+timeleft -= 1;
+}, 1000);
+
 
 function showQuestion(questions) {
     for (var i=0; i < questions.length; i++) {
@@ -34,17 +49,7 @@ function showQuestion(questions) {
     }
 }
 
-function setTime() {
-    var timerInterval = setInterval(function(){
-        timeLeft--;
-        timeEl.textContent = "Time remaining: " + timeLeft;
 
-        if(timeLeft === 0) {
-            clearInterval(timerInterval);
-            endQuiz();
-        }
-    }, 60000);
-}
 
 // function endQuiz() {
 //     timeEl.textContent = " ";
@@ -76,73 +81,53 @@ var questions = [
     {
         question: "In the array var countries = [“Jordan”, “Finland”, “Morocco”, “South Africa”], which country is index[2]?",
         answers: [
-            {text: "Jordan", correct: false},
-            {text: "Finland", correct: false},
-            {text: "Morocco", correct: true},
-            {text: "South Africa", correct: false},
-        ]
+            {a: "Jordan", correct: false},
+            {b: "Finland", correct: false},
+            {c: "Morocco", correct: true},
+            {d: "South Africa", correct: false},
+        ],
+        correctAnswer: "c"
     },
     {
         question: "Which of the following is in the correct format for Javascript?",
         answers: [
-            {text: "going-places", correct: false},
-            {text: "going_places", correct: false},
-            {text: "going places", correct: false},
-            {text: "goingPlaces", correct: true},
-        ]
+            {a: "going-places", correct: false},
+            {b: "going_places", correct: false},
+            {c: "going places", correct: false},
+            {d: "goingPlaces", correct: true},
+        ],
+        correctAnswer: "d"
     },
     {
         question: "Where would you start when traversing the DOM?",
         answers: [
-            {text: "body", correct: false},
-            {text: "childNode", correct: false},
-            {text: "document", correct: true},
-            {text: "children", correct: false},
-        ]
+            {a: "body", correct: false},
+            {b: "childNode", correct: false},
+            {c: "document", correct: true},
+            {d: "children", correct: false},
+        ],
+        correctAnswer: "c"
     },
     {
         question: "Which of the following is incorrect?",
         answers: [
-            {text: "DOM = document object model", correct: false},
-            {text: "API = application programming index", correct: true},
-            {text: "GUI = graphic user interface", correct: false},
-            {text: "HTML = hypertext markup language", correct: false},
-        ]
+            {a: "DOM = document object model", correct: false},
+            {b: "API = application programming index", correct: true},
+            {c: "GUI = graphic user interface", correct: false},
+            {d: "HTML = hypertext markup language", correct: false},
+        ],
+        correctAnswer: "b"
     },
     {
         question: "What should you use for your code to read a button being “clicked?",
         answers: [
-            {text: "addEventListener", correct: true},
-            {text: "querySelector", correct: false},
-            {text: "setAttribute", correct: false},
-            {text: "getElementById", correct: false},
-        ]
+            {a: "addEventListener", correct: true},
+            {b: "querySelector", correct: false},
+            {c: "setAttribute", correct: false},
+            {d: "getElementById", correct: false},
+        ],
+        correctAnswer: "a"
     }
 
 
 ]
-
-// if (questionOneEl ==="Morocco") {
-//     correct++;
-// }
-// if (questionTwoEl ==="goingPlaces") {
-//     correct++;
-// }
-// if (questionThreeEl ==="document") {
-//     correct++;
-// }
-// if (questionFourEl ==="API = application programming index") {
-//     correct++;
-// }
-// if (questionFiveEl ==="addEventListener") {
-//     correct++;
-// }
-
-
-
-
-
-
-
-// document.getElementById("after-submit").style.visibility = "visible";
-// document.getElementById("number_correct").innerHTML = "You got " + correct + " out of 5 questions correct";
