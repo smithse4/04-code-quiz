e665ef3c1ca2c1ef80126b8040ac6e5ab23c3e36
Stephanie smith <smithse4@gmail.com>
2020-03-11 17:19:41 -0400

    updated start and clock functions

--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -2,51 +2,51 @@
 var questions = [
     {
         question: "In the array var countries = [“Jordan”, “Finland”, “Morocco”, “South Africa”], which country is index[2]?",
-        answers: [
-            {a: "Jordan"},
-            {b: "Finland"},
-            {c: "Morocco"},
-            {d: "South Africa"},
+        choices: [
+            "Jordan",
+            "Finland",
+            "Morocco",
+            "South Africa",
         ],
-        correctAnswer: "c"
+        answer: "Morocco"
     },
     {
         question: "Which of the following is in the correct format for Javascript?",
-        answers: [
-            {a: "going-places"},
-            {b: "going_places"},
-            {c: "going places"},
-            {d: "goingPlaces"},
+        choices: [
+            "going-places",
+            "going_places",
+            "going places",
+            "goingPlaces",
         ],
-        correctAnswer: "d"
+        answer: "goingPlaces"
     },
     {
         question: "Where would you start when traversing the DOM?",
-        answers: [
-            {a: "body"},
-            {b: "childNode"},
-            {c: "document"},
-            {d: "children"},
+        choices: [
+            "body",
+            "childNode",
+            "document",
+            "children",
         ],
-        correctAnswer: "c"
+        answer: "document"
     },
     {
         question: "Which of the following is incorrect?",
-        answers: [
-            {a: "DOM = document object model"},
-            {b: "API = application programming index"},
-            {c: "GUI = graphic user interface"},
-            {d: "HTML = hypertext markup language"},
+        choices: [
+            "DOM = document object model",
+            "API = application programming index",
+            "GUI = graphic user interface",
+            "HTML = hypertext markup language",
         ],
-        correctAnswer: "b"
+        answer: "API = application programming index"
     },
     {
         question: "What should you use for your code to read a button being “clicked?",
-        answers: [
-            {a: "addEventListener"},
-            {b: "querySelector"},
-            {c: "setAttribute"},
-            {d: "getElementById"},
+        choices: [
+            "addEventListener",
+            "querySelector",
+            "setAttribute",
+            "getElementById",
         ],
-        correctAnswer: "a"
+        answer: "addEventListener"
     }]
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,53 +1,82 @@
-// Start, question, and answer buttons
+// Start and question buttons and containers
 var startButton = document.getElementById("btn-start");
 var startTextEl = document.getElementById("start-text");
-var answerButton = document.querySelector(".answer-btn");
 var questionContainerEl = document.getElementById("question-container");
 var questionEl = document.getElementById("question");
-var answerBtnEl = document.getElementById("answer-btns");
+var choicesEl = document.getElementById("choices");
+// Timer
+var time = 60;
+var timerID;
+var timeEl = document.getElementById("countdown")
 
 
-
-let questionIndex
+var currentQuestionIndex = 0
 
 
 
 // Respond to buttons being vlicked
 startButton.addEventListener("click", startQuiz)
-answerButton.addEventListener("click", () => {
-    questions[i]++;
-    setNextQuestion;
-})
+// answerButton.addEventListener("click", () => {
+//     questions[i]++;
+//     setNextQuestion;
+// })
 
-// Start quiz - remove opening message and start button to move into first question
+// Start quiz - pressing "start" button results in opening message changing to first question and timer initializes.
 function startQuiz() {
     startButton.classList.add("hide");
     startTextEl.classList.add("hide");
-    questionsIndex = 0;
     questionContainerEl.classList.remove("hide");
+    // Initialize countdown timer
+    timerId = setInterval(clock, 1000);
+    timeEl.textContent = time;
+    // Move from start view to the first question
     showQuestion();
-   
-    
 }
 
-// Timer -- need to get set to start button
-var timeleft = 60;
-var timer = setInterval(function(){
-if(timeleft <= 0){
-    clearInterval(timer);
-    document.getElementById("time").innerHTML = "Quiz over";
-} else {
-    document.getElementById("time").innerHTML = "Time remaining: " + timeleft;
-}
-timeleft -= 1;
-}, 1000);
-
-
-function showQuestion(questions) {
-    for (var i=0; i < questions.length; i++) {
-        questionEl.innerText = question.question;
+function clock() {
+    // track time
+    time--;
+    timeEl.textContent = time;
+    // If user runs out of time before finishing quix, end quiz
+    if (time <= 0) {
+        quizEnd();
     }
 }
+// Timer -- need to get set to start button
+// var timeleft = 60;
+// var timer = setInterval(function(){
+// if(timeleft <= 0){
+//     clearInterval(timer);
+//     document.getElementById("time").innerHTML = "Quiz over";
+// } else {
+//     document.getElementById("time").innerHTML = "Time remaining: " + timeleft;
+// }
+// timeleft -= 1;
+// }, 1000);
+
+// Function to populate questions from our question array.
+function showQuestion() {
+    // Find question from array of questions
+    var currentQuestion = questions[currentQuestionIndex];
+    // Fill in question
+    var titleEl = document.getElementById("question-slot");
+    titleEl.textContent = currentQuestion.title;
+    choicesEl.innerHTML = "";
+
+    currentQuestion.choices.forEach(function(choice, i) {
+        var choiceNode = document.createElement("button");
+        choiceNode.setAttribute("class", "choice");
+        choiceNode.setAttribute("value", choice);
+        choiceNode.textContent = i + 1 + ". " + choice;
+        // Make new choice buttons click listenter
+        choiceNode.onclick = clickChoiceButton;
+        // Append choice so it displays on page
+        choicesEl.appendChild(choiceNode);
+    });
+}
+
+// Function to react to question choice being clicked. Depending on quiz status could move to next question or end quiz. Will also deduct time from timer if question is answered incorrectly.
+
 
 
 
